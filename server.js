0553f889e9f00cb67a1441670ad9e43ae2a6e5c3
Mrigendra Kumar <mrigendra.kumar@prodigi.local>
2017-08-22 19:06:22 +0530

    changes

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ var path = require("path"),
     express = require("express");
     var GMB = require('google_my_business')
  var axios = require('axios');
-
+    var fs = require('fs');
  var engine = require('consolidate');
- 
+ global.stores = [];
 
- 
+    var funcWrapper = (placedId) =>{
+        axios.get('https://maps.googleapis.com/maps/api/place/details/json?placeid='+placedId+'&key=AIzaSyBf8sD87XQumevDtICVowDRmo-u1jsus7o')
+        .then(function(response){
+            return response;
+        })
+    }
     // var YOUR_CLIENT_ID = 'rUzayh27Ilek6gbKkgmM9bqH', YOUR_CLIENT_SECRET = '83848323099-2r2u81ach2aad9kb86gd66akvkovri8s.apps.googleusercontent.com',
     // YOUR_REDIRECT_URL = 'https://accounts.google.com/o/oauth2/auth';
 
@@ -19,15 +24,18 @@ var path = require("path"),
     // );
 
 
-var DIST_DIR = path.join(__dirname, "dist"), SRC_DIR = path.join(__dirname, "src"),  
+var DIST_DIR = path.join(__dirname, "dist"), SRC_DIR = path.join(__dirname, "src"),  SRC_PAGE_DIR = path.join(SRC_DIR, "pages"),  
     PORT = 3000,
     app = express();
 
 //Serving the files on the dist folder
 app.use(express.static(DIST_DIR));
 
-app.set('views', __dirname + '/dist');
-app.engine('html', engine.mustache);
+// app.set('views', __dirname + '/dist');
+// app.engine('html', engine.mustache);
+
+app.set('view engine', 'ejs');
+
 
 //Send index.html when the user access the web
 app.get("/main", function (req, res) {  
@@ -57,15 +65,43 @@ app.get("/main", function (req, res) {
 // });
 
 
-
-app.get("/location", function(req, res){
-    console.log('enter test');
+app.get('/api/list', function(req, res){
     axios.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyBf8sD87XQumevDtICVowDRmo-u1jsus7o')
     .then(function(response){
         console.log('response', response.data.result);
-        return res.render(path.join(SRC_DIR, "location.html"), JSON.stringify({reviews: response.data.result.address_components}))
+        return response.data;
+    })
+})
+
+
+app.get("/location", function(req, res){
+    console.log('enter test');
+    axios.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJKR-7EDf9DDkR7g4gVBFioDo&key=AIzaSyBf8sD87XQumevDtICVowDRmo-u1jsus7o')
+    .then(function(response){
+        var store = response.data.result || {};
+        global.stores.push(store); 
+        // console.log('response', response.data.result);
+        res.render(path.join(SRC_PAGE_DIR, 'list'), {store: store});
+        // res.send(reviews);
+        // fs.readFile(path.join(SRC_DIR, "location.html"), 'utf8', function(err, text){
+        //     res.send(reviews);
+        // });
     })
 });
+
+app.get("/detail", function(req, res){//console.log('store', global.stores[0]); 
+let d = 'ddd', ar = ['ChIJKR-7EDf9DDkR7g4gVBFioDo'];
+    var funcList = ar.map((item)=>{
+        var p = new Promise((resolve, reject) => {
+            return funcWrapper(item);
+        }); 
+        return p;
+    });console.log('funclist', funcList);
+    Promise.all(funcList).then(values =>{
+        console.log(values);
+    })
+    res.render(path.join(SRC_PAGE_DIR, 'detail'), {store: global.stores[0]});
+})
 
 
 app.listen(process.env.PORT || PORT);
